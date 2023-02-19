d492ee1129ee11df8ea59ace7907f561e53609b9
Bailey Seely <baileyxdxd@gmail.com>
2023-02-19 00:34:29 -0400

    general functionality

--- Dog.js
+++ Dog.js
@@ -1,1 +1,30 @@
-// Create the Dog class here
+import dogs from '/data.js'
+
+class Dog {
+
+    constructor(data) {
+        Object.assign(this, data)
+    }
+
+    setMatchStatus(bool) {
+        this.hasBeenLiked = bool
+        this.hasBeenSwiped = true
+    }
+    
+    getDogHtml() {
+        const {name, avatar, age, bio} = this
+        return `
+            <img src="${avatar}" class="dog">
+            <div class="action hidden" id="hidden">
+                <img src="/images/badge-like.png">
+            </div>
+            <div class="text">
+                <h1>${name}, ${age}</h1>
+                <p>How you doin?</p>
+            </div>
+                `
+    }
+}
+
+export default Dog
+
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Remember to import the data and Dog class!
+import data from '/data.js'
+import Dog from '/Dog.js'
+
+let currentDogIndex = 0
+let currentDog = new Dog(data[currentDogIndex])
+document.getElementById('liked').addEventListener('click', yes)
+document.getElementById('swiped').addEventListener('click', no)
+
+render()
+
+function render() {
+    document.getElementById('container').innerHTML = currentDog.getDogHtml()
+
+    if (currentDogIndex >= data.length) {
+        document.body.innerHTML = `<h1 class="end">No dogs left in your area!</h1>                         
+                                  `
+    }
+}
+
+function getNewDog() {
+    currentDogIndex++
+    currentDog = new Dog(data[currentDogIndex])
+    render()
+}
+
+function yes() {
+    currentDog.setMatchStatus(true)
+    console.log(document.getElementById('hidden'))
+    document.getElementById('hidden').classList.remove('hidden')
+    setTimeout(() => {
+        getNewDog()
+    }, 2000)
+}
+
+function no() {
+    currentDog.setMatchStatus(false)
+    setTimeout(() => {
+        getNewDog()
+    }, 2000)
+}
+
